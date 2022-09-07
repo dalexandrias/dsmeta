4e6c8f28c6f14a1db2c03b34f15490d73d73e520
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-09-07 10:55:12 -0300

    Project Created

---
+++ backend/src/main/java/com/metas/dsmetas/DsmetasApplication.java
@@ -0,0 +1,13 @@
+package com.metas.dsmetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/metas/dsmetas/DsmetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metas.dsmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
