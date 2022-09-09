4605278b34bf51949ee4f74cb12a1f525af3bf66
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2022-09-09 00:04:07 -0300

    Database

---
+++ backend/src/main/java/com/metas/dsmetas/entities/Sale.java
@@ -0,0 +1,69 @@
+package com.metas.dsmetas.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private String visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    public Sale() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getVisited() {
+        return visited;
+    }
+
+    public void setVisited(String visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
